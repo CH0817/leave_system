c71486501c6e8b9e413a15917cb55102242d7a1a
Rex Yu <yu.chenhang@gmail.com>
2019-05-22 15:29:12 +0800

    create test sql

---
+++ src/main/resources/db/data/data-dev-company.sql
@@ -0,0 +1,2 @@
+INSERT INTO company (id, name, create_date) VALUES ('a', 'test company', NOW());
+INSERT INTO company (id, name, create_date) VALUES ('b', 'test company2', NOW());
---
+++ src/main/resources/db/data/data-dev-employee.sql
@@ -0,0 +1,1 @@
+INSERT INTO employee (id, name, password, company_id, arrive_date, create_date) VALUES ('a', '測試', 'a', 'a', NOW(), NOW())
