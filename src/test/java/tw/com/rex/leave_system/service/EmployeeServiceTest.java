5abac23d5c7c7c4f7d94e3f2bb4d47bd41b3b6f0
Rex Yu <yu.chenhang@gmail.com>
2019-06-11 15:01:57 +0800

    add enable security annotation

--- src/test/java/tw/com/rex/leave_system/service/EmployeeServiceTest.java
+++ src/test/java/tw/com/rex/leave_system/service/EmployeeServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@EnableWebSecurity
 @Transactional
 @Sql({"/db/data/data-dev-company.sql", "/db/data/data-dev-employee.sql"})
 public class EmployeeServiceTest {
